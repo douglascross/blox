9288f8178156de37a8a738d2f809ad37288de0c7
Douglas Cross <brother.douglas@gmail.com>
2016-05-07 23:10:26 +1000

    Update README.

--- README.md
+++ README.md
@@ -7,16 +7,26 @@ So this is not ready for commercial use.
 
 Example
 ```javascript
-//...
-var csg = { shape: 'circle', radius: 120,
-  subtract: [
-    { shape: 'box', x: 200, y: 200, z: 200 }
-  ]
+var csg = {
+    subtract: [
+        {
+            intersect: [
+                { shape: 'sphere', radius: 135, color: 0x0000ff },
+                { shape: 'box', x: 200, y: 200, z:200, color: 0xff0000 }
+            ]
+        },
+        {
+            union: [
+                { shape: 'cylinder', top: 70, bottom: 70, height: 200, color: 0x00ff00 },
+                { shape: 'cylinder', top: 70, bottom: 70, height: 200, color: 0x00ff00, ops: [{rotate: [90, 0, 0]}] },
+                { shape: 'cylinder', top: 70, bottom: 70, height: 200, color: 0x00ff00, ops: [{rotate: [90, 90, 0]}] }
+            ]
+        }
+    ]
 };
-var geometry = BLOX.toGeometry(csg);
-//...
+var mesh = BLOX.toMesh(csg);
 ```
 
 [See it in action here.](http://douglascross.github.io/blox/)
 
-![alt tag](http://douglascross.github.io/blox/images/preview.png?2)
+![alt tag](http://douglascross.github.io/blox/images/preview.png?3)
--- app/scripts/examples/color.js
+++ app/scripts/examples/color.js
@@ -1,39 +1,17 @@
-var colorExample = {
-    spherePart: {
-        shape: 'sphere',
-        radius: 135,
-        color: 0x0000ff
-    },
-    boxPart: {
-        shape: 'box',
-        x: 200, y: 200, z:200,
-        color: 0xff0000
-    },
-    cylinderPart: {
-        shape: 'cylinder',
-        top: 70,
-        bottom: 70,
-        height: 200,
-        color: 0x00ff00
-    }
-};
-colorExample.sphereWithBox = {
-    intersect: [
-        colorExample.spherePart,
-        colorExample.boxPart
-    ]
-};
-colorExample.cylindersPart = {
-    union: [
-        {shape: 'cylinder', top: 70, bottom: 70, height: 200},
-        {shape: 'cylinder', top: 70, bottom: 70, height: 200, ops: [{rotate: [90, 0, 0]}]},
-        {shape: 'cylinder', top: 70, bottom: 70, height: 200, ops: [{rotate: [90, 90, 0]}]}
-    ],
-    color: 0x00ff00
-};
-colorExample.finalStandard = {
+var classicExample = {
     subtract: [
-        colorExample.sphereWithBox,
-        colorExample.cylindersPart
+        {
+            intersect: [
+                { shape: 'sphere', radius: 135, color: 0x0000ff },
+                { shape: 'box', x: 200, y: 200, z:200, color: 0xff0000 }
+            ]
+        },
+        {
+            union: [
+                { shape: 'cylinder', top: 70, bottom: 70, height: 200, color: 0x00ff00 },
+                { shape: 'cylinder', top: 70, bottom: 70, height: 200, color: 0x00ff00, ops: [{rotate: [90, 0, 0]}] },
+                { shape: 'cylinder', top: 70, bottom: 70, height: 200, color: 0x00ff00, ops: [{rotate: [90, 90, 0]}] }
+            ]
+        }
     ]
 };
